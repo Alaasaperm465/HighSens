6d880b813b4d8d9cd492c7b7e0210a5fd8ea22cd
alaasaber <alaasabermostafa9@gmail.com>
2026-02-14 21:16:53 +0200

    login page

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f8367fb310913a5f6eae760510b34bb70a3ae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f54c5cb75a691ab6362c40a8496d65d3beecb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
